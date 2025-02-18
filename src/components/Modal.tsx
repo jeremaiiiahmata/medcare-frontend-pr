3a1f186f421168c6a9d3627ce1c998e2cc38147c
Jeremaiah Mata <129203757+jeremaiiiahmata@users.noreply.github.com>
2025-02-18 16:07:34 +0800

    Fix landing page and add tab logo

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -10,11 +10,11 @@ interface Props {
 const Modal = ({ title, children, setIsOpen }: Props) => {
   return (
     <div className="inset-0 items-center justify-center flex h-full w-full bg-black/70 absolute z-10 ">
-      <div className="bg-white h-fit w-[32rem] p-2 rounded-lg">
+      <div className="bg-white h-fit w-[48rem] p-4 rounded-lg">
         <div className="flex justify-between items-center">
           <h2 className="text-xl font-semibold">{title}</h2>
           <IoIosClose
-            size={24}
+            size={32}
             onClick={() => {
               setIsOpen(false);
             }}
